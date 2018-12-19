23c5320a251b791cb7a4ccd53d9cc95501091a52
MiguelRibeiro65 <paradix65@gmail.com>
2018-12-19 18:07:50 +0000

    povoamento

---
+++ povoamento.sql
@@ -0,0 +1,103 @@
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (1, 'Nert Ropkins','admin', 'admin1@cf.com', 'admin1', 'admin1', '661-584-3515');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (2, 'Sasha Oxterby','admin', 'admin2@cf.com', 'admin2', 'admin2', '660-359-7920');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (3, 'Maison Cudmore','admin', 'admin3@cf.com', 'admin3', 'admin3', '546-786-9293');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (4, 'Esme McKnish','admin', 'admin4@cf.com', 'admin4', 'admin4', '718-759-5198');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (5, 'Harrietta Snelle','admin', 'admin5@cf.com', 'admin5', 'admin5', '870-843-8160');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (6, 'Lorry Nutter', 'admin', 'admin6@cf.com', 'admin6', 'admin6', '755-188-6587');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (7, 'Tracy Baskerville','admin', 'admin7@cf.com', 'admin7', 'admin7', '203-293-2738');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (8, 'Verney Hebblewaite','admin', 'admin8@cf.com', 'admin8', 'admin8', '964-692-5578');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (9, 'Avigdor Rotter', 'admin', 'admin9@cf.com', 'admin9', 'admin9', '643-413-7143');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (10, 'Eydie Garaway', 'admin', 'admin10@cf.com', 'admin10', 'admin10', '355-897-7477');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (11, 'Granny Stainburn','func', 'func1@cf.com', 'func1', 'func1', '286-146-3034');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (12, 'Isabel Lohmeyer', 'func','func2@cf.com', 'func2', 'func2', '690-305-7649');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (13, 'Rubie Pirolini', 'func','func3@cf.com', 'func3', 'func3', '915-788-9306');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (14, 'Arleyne Barron', 'func','func4@cf.com', 'func4', 'func4', '979-148-0973');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (15, 'Neils Guihen', 'func','func5@cf.com', 'func5', 'func5', '682-137-9303');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (16, 'Dirk Gilcrist', 'func','func6@cf.com', 'func6', 'func6', '669-445-0950');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (17, 'Margeaux Piatkow', 'func','func7@cf.com', 'func7', 'func7', '475-127-2189');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (18, 'Timothea Judkins', 'func','func8@cf.com', 'func8', 'func8', '915-996-8383');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (19, 'Karin Ramsier', 'func','func9@cf.com', 'func9', 'func9', '877-264-0655');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (20, 'Chan Crosfeld', 'func','func10@cf.com', 'func10', 'func10', '863-311-3610');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (21, 'Teirtza Abatelli','fab', 'fab1@cf.com', 'fab1', 'fab1', '562-865-2716');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (22, 'Monica Wooles', 'fab','fab2@cf.com', 'fab2', 'fab2', '152-556-7808');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (23, 'Malena Hacun', 'fab','fab3@cf.com', 'fab3', 'fab3', '285-963-7950');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (24, 'Margery Hatley', 'fab','fab4@cf.com', 'fab4', 'fab4', '547-525-7069');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (25, 'Phip Whitnall', 'fab','fab5@cf.com', 'fab5', 'fab5', '395-340-1686');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (26, 'Zebulen Hewins', 'fab','fab6@cf.com', 'fab6', 'fab6', '987-600-4922');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (27, 'Darwin Croce', 'fab','fab7@cf.com', 'fab7', 'fab7', '493-888-0408');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (28, 'Iggy Corpes', 'fab','fab8@cf.com', 'fab8', 'fab8', '152-243-5349');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (29, 'Dana MacCaig', 'fab','fab9@cf.com', 'fab9', 'fab9', '827-882-8286');
+insert into Utilizadores (id, nome, tipo, mail, username, password, contacto) values (30, 'Sky Densell', 'fab','fab10@cf.com', 'fab10', 'fab10', '201-398-8127');
+
+insert into Items (id, tipo, nome, preco, quantidade) values (1, 'modelo', 'Série 1 - 3 portas',20000, 5);
+insert into Items (id, tipo, nome, preco, quantidade) values (2, 'modelo', 'Série 1 - 5 portas',22000, 2);
+insert into Items (id, tipo, nome, preco, quantidade) values (3, 'modelo', 'Série 2 Coupé',24000, 1);
+insert into Items (id, tipo, nome, preco, quantidade) values (4, 'modelo', 'Série 2 Cabrio',22000, 4);
+insert into Items (id, tipo, nome, preco, quantidade) values (5, 'modelo', 'Série 3 Grand Turismo',30000, 10);
+insert into Items (id, tipo, nome, preco, quantidade) values (6, 'modelo', 'Série 4 Gran Coupé', 32000, 5);
+insert into Items (id, tipo, nome, preco, quantidade) values (7, 'modelo', 'Série 5 Berlina',35000, 6);
+insert into Items (id, tipo, nome, preco, quantidade) values (8, 'modelo', 'Série 6 Gran Turismo',52000, 3);
+insert into Items (id, tipo, nome, preco, quantidade) values (9, 'modelo', 'Série 7','79000', '8');	
+insert into Items (id, tipo, nome, preco, quantidade) values (10, 'modelo', 'Série 7 Longo',120000, 2);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (11, 'pintura', 'preto',300, 2);
+insert into Items (id, tipo, nome, preco, quantidade) values (12, 'pintura', 'branco',300, 5);
+insert into Items (id, tipo, nome, preco, quantidade) values (13, 'pintura', 'azul',300, 6);
+insert into Items (id, tipo, nome, preco, quantidade) values (14, 'pintura', 'verde',300, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (15, 'pintura', 'amarelo',300, 10);
+insert into Items (id, tipo, nome, preco, quantidade) values (16, 'pintura', 'cinzento',300, 20);
+insert into Items (id, tipo, nome, preco, quantidade) values (17, 'pintura', 'castanho',300, 5);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (18, 'motor', 'VEE',1100, 6);
+insert into Items (id, tipo, nome, preco, quantidade) values (19, 'motor', 'INLINE',1760, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (20, 'motor', 'STRAIGHT',1300, 4);
+insert into Items (id, tipo, nome, preco, quantidade) values (21, 'motor', 'VR and W',2000, 3);
+insert into Items (id, tipo, nome, preco, quantidade) values (22, 'motor', 'BOXER',4570, 5);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (23, 'pacote', 'Desportivo M',5600, 5);
+insert into Items (id, tipo, nome, preco, quantidade) values (24, 'pacote', 'Confort',4570, 4);
+insert into Items (id, tipo, nome, preco, quantidade) values (25, 'pacote', 'Line Luxury',7800, 3);
+insert into Items (id, tipo, nome, preco, quantidade) values (26, 'pacote', 'Line Advantage',4700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (27, 'pacote', 'Line Urban',3200, 7);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (28, 'jantes', '17\'\'',1200, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (29, 'jantes', '18\'\'',1400, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (30, 'jantes', '19\'\'',1600, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (31, 'jantes', '20\'\'',1800, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (32, 'jantes', '21\'\'',2000, 7);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (33, 'pneus', 'Continental',1000, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (34, 'pneus', 'Goodyear',900, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (35, 'pneus', 'Michelin',1100, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (36, 'pneus', 'Bridgestone',1300, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (37, 'pneus', 'Firestone',2000, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (38, 'pneus', 'Uniroyal',700, 7);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (39, 'Tecido Move Antracite', 'Uniroyal',1200, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (40, 'Pele Dakota reto', 'Uniroyal',900, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (41, 'Pele Dakota Castanho', 'Uniroyal',1000, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (42, 'estofos', 'Tecido Corner preto',2500, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (43, 'estofos', 'Tecido/Pele prata',4600, 7);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (44, 'frisos', 'Prata Satin mate',1000, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (45, 'frisos', 'Alumínio Escolvado',1100, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (46, 'frisos', 'Alumínio Hexagon',2000, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (47, 'frisos', 'Branco Acrílico',6000, 7);
+
+insert into Items (id, tipo, nome, preco, quantidade) values (48, 'extras', 'Função de TV',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (49, 'extras', 'Consola traseira',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (50, 'extras', 'Sistema de som Surrond',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (51, 'extras', 'Spoiler traseiro',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (52, 'extras', 'Vidros com proteção solar',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (53, 'extras', 'Travões desportivos',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (54, 'extras', 'Volante aquecido',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (55, 'extras', 'Vidros laminados',700, 7);
+insert into Items (id, tipo, nome, preco, quantidade) values (56, 'extras', 'Escovas faróis',700, 7);
+
+
+
+
+
+
+
+
